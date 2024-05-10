11b15d99ee60b9116d3622e7a4fc7f6c1538cae6
Daniel Santiso Candel <danielsantiso04@gmail.com>
2024-05-10 19:50:07 +0200

    OAuth puesto

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+    $clientID = '913954504107-vggh9iegtg245ue33001j7tf5cogmpea.apps.googleusercontent.com';
+    $clientSecret = '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';
+    $redirectUri = 'http://localhost:80/MAD-TFG/login.php';
+?>
--- index.php
+++ index.php
@@ -1,60 +1,25 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Riot Merch</title>
-    <link rel="stylesheet" href="index.css">
-    <script src="https://kit.fontawesome.com/76fb5d8fe4.js" crossorigin="anonymous"></script>
+    <title>Inicio</title>
+    <?php
+    require_once 'vendor/autoload.php';
+    require_once 'config.php';
+     
+    $client = new Google_Client();
+    $client->setClientId($clientID);
+    $client->setClientSecret($clientSecret);
+    $client->setRedirectUri($redirectUri);
+    $client->addScope("email");
+    $client->addScope("profile");
+    
+    
+    echo "<a href='".$client->createAuthUrl()."'>Google Login</a>";
+    ?>
 </head>
 <body>
-<header>
-    <div>
-        <a href="index.php"><img id="logo" src="img/ri8566rddb-riot-games-logo-riot-games-logo-download-vector.png"></a>
-    </div>
-    <div>
-        <a href="carrito.php">Carrito</a>
-        <?php
-        if (isset($_SESSION["User"])) {
-            echo "<a href=\"Perfil.php\"><p>Mi Perfil</p></a>";
-        } else {
-            echo "<a href=\"inicio.php\"><p>Iniciar sesión</p></a>";
-        }
-        ?>
-    </div>
-</header>
-<div id="venta">
-    <?php
-    include "Conexion.php";
-    $select = $conn->prepare("SELECT * FROM producto");
-    $select->execute();
-    $resultado = $select->get_result();
-    while ($fila = $resultado->fetch_assoc()) {
-        $ID = $fila["ID"];
-        $Nombre = $fila["Nombre"];
-        $foto = $fila["foto"];
-        $precio = $fila["precio"];
-        echo "<div class=\"producto\">";
-        echo "<a href=\"producto.php?id=$ID\">";
-        echo "<div>";
-        echo "<img src=\"img/Productos/$foto.png\">";
-        echo "</div>";
-        echo "<p>$Nombre</p>";
-        echo "<p id=\"precio\">$precio</p>";
-        echo "</a>";
-        echo "</div>";
-    }    
-    ?>
-</div>
+    
 </body>
-<footer>
-    <div><img src="img/ri8566rddb-riot-games-logo-riot-games-logo-download-vector.png" alt=""></div>
-    <div><ul>
-        <li>
-            <a href="https://es-es.facebook.com/"><i class="fa-brands fa-facebook"></i></a>
-        </li>
-        <li><a href="https://www.instagram.com/"><i class="fa-brands fa-instagram"></i></a></li>
-        <li><a href="https://www.youtube.com/"><i class="fa-brands fa-youtube"></i></a></li>
-    </ul></div>
-</footer>
 </html>
