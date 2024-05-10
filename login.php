e6edbdee177525cc56a4687942a11b58796d12dd
Daniel Santiso Candel <danielsantiso04@gmail.com>
2024-05-10 21:34:35 +0200

    commit

--- login.php
+++ login.php
@@ -22,13 +22,11 @@
     $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
     $client->setAccessToken($token['access_token']);
 
-    // get profile info
     $google_oauth = new Google_Service_Oauth2($client);
     $google_account_info = $google_oauth->userinfo->get();
     $email =  $google_account_info->email;
     $name =  $google_account_info->name;
 
-    // Estos datos son los que obtenemos....	
     echo $email . '<br>';
     echo $name;
   }
