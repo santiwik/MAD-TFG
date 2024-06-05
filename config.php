7849dfa7c7291818063f329cb93978deca974691
Daniel Santiso Candel <danielsantiso04@gmail.com>
2024-06-05 12:12:54 +0200

    Commit actualizacion de link

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
     $clientID = '913954504107-vggh9iegtg245ue33001j7tf5cogmpea.apps.googleusercontent.com';
     $clientSecret = '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';
-    $redirectUri = 'https://12af-88-26-8-191.ngrok-free.app/profile.php';
+    $redirectUri = 'https://4cfc-88-26-8-191.ngrok-free.app';
 ?>
